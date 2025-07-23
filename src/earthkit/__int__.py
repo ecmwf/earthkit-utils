23e238b17c049286e4182aa0cb0d1b617f7d1e0d
oisin-m <oisinmmm@gmail.com>
2025-07-23 10:12:56 +0200

    please precommit

--- src/earthkit/__int__.py
+++ src/earthkit/__int__.py
@@ -39,7 +39,8 @@ def __getattr__(name):
             mod = importlib.import_module(f"{__name__}.{name}")
         except Exception as e:
             raise AttributeError(
-                f"Module '{__name__}' has no attribute '{name}' " f"(failed to import '{__name__}.{name}'): {e}"
+                f"Module '{__name__}' has no attribute '{name}' "
+                f"(failed to import '{__name__}.{name}'): {e}"
             ) from e
         globals()[name] = mod
         return mod
