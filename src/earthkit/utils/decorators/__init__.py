1c45f8aee5387448fc352819f82a98adfb2dfb4e
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2026-01-30 15:37:24 +0000

    basic decorator placeholders

--- src/earthkit/utils/decorators/__init__.py
+++ src/earthkit/utils/decorators/__init__.py
@@ -10,11 +10,13 @@
 from .thread_handlers import thread_safe_cached_property
 
 try:
-    from earthkit.data.decorators import format_handler
+    from earthkit.data.decorators import format_handler, metadata_handler
 except ImportError:
     from .format_handler import format_handler
+    from .metadata_handler import metadata_handler
 
 __all__ = [
     "thread_safe_cached_property",
     "format_handler",
+    "metadata_handler",
 ]
