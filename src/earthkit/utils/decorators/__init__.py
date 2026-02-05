fd0089690235c51fad41c78ac9cbd73439494c8a
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2026-02-05 15:31:36 +0000

    format_handler code

---
+++ src/earthkit/utils/decorators/__init__.py
@@ -0,0 +1,25 @@
+# (C) Copyright 2025 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+# try:
+#     print("Trying")
+#     from earthkit.data.utils.decorators.format_handler import format_handler
+#     from earthkit.data.utils.decorators.metadata_handler import metadata_handler
+#     print("Imported from earthkit-data")
+# except ImportError as e:
+#     print(f"Falling back to ek-utils implementations: {e}")
+from .format_handlers import format_handler
+from .metadata_handlers import metadata_handler
+from .thread_handlers import thread_safe_cached_property
+
+__all__ = [
+    "thread_safe_cached_property",
+    "format_handler",
+    "metadata_handler",
+]
