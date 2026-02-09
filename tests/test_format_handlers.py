5c812ad79d8c8d14cc6e36f6397dd4c1b202c359
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2026-02-09 09:30:29 +0000

    QA

--- tests/test_format_handlers.py
+++ tests/test_format_handlers.py
@@ -6,68 +6,89 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 #
-import pytest
-
 import numpy as np
+import pytest
 import xarray as xr
-# from earthkit.data import from_source, from_object
 
 from earthkit.utils.decorators import format_handler
+
+# from earthkit.data import from_source, from_object
+
 
 TEST_NUMPY = np.array([1, 2, 3])
 TEST_DATAARRAY = xr.DataArray(TEST_NUMPY, name="test", dims=["x"], coords={"x": [0, 1, 2]})
 TEST_DATASET = xr.Dataset({"test": TEST_DATAARRAY})
 
 
-@pytest.mark.parametrize("in_data", [
-    TEST_NUMPY,
-    TEST_DATAARRAY,
-    TEST_DATASET,
-])
+@pytest.mark.parametrize(
+    "in_data",
+    [
+        TEST_NUMPY,
+        TEST_DATAARRAY,
+        TEST_DATASET,
+    ],
+)
 def test_format_handler_numpy(in_data):
     @format_handler()
     def _numpy_handler(data: np.ndarray):
         return data
+
     assert isinstance(_numpy_handler(in_data), np.ndarray)
 
 
-@pytest.mark.parametrize("in_data", [
-    TEST_NUMPY,
-    TEST_DATAARRAY,
-    TEST_DATASET,
-])
+@pytest.mark.parametrize(
+    "in_data",
+    [
+        TEST_NUMPY,
+        TEST_DATAARRAY,
+        TEST_DATASET,
+    ],
+)
 def test_format_handler_xarray(in_data):
     @format_handler()
     def _xarray_handler(data: xr.DataArray):
         return data
+
     assert isinstance(_xarray_handler(in_data), xr.DataArray)
 
 
-@pytest.mark.parametrize("in_data", [
-    TEST_NUMPY,
-    TEST_DATAARRAY,
-    TEST_DATASET,
-])
+@pytest.mark.parametrize(
+    "in_data",
+    [
+        TEST_NUMPY,
+        TEST_DATAARRAY,
+        TEST_DATASET,
+    ],
+)
 def test_format_handler_multiple_accepted(in_data):
     """All in_data types are accepted, therefore no tranformation should be attempted."""
+
     @format_handler()
     def _xarray_handler(data: xr.DataArray | np.ndarray | xr.Dataset):
         return data
+
     assert isinstance(_xarray_handler(in_data), type(in_data))
 
 
-@pytest.mark.parametrize("in_data", [
-    TEST_NUMPY,
-    TEST_DATAARRAY,
-    TEST_DATASET,
-])
-@pytest.mark.parametrize("param", [
-    1,
-    "2",
-    # 3.  # TODO: Not yet implemented
-])
+@pytest.mark.parametrize(
+    "in_data",
+    [
+        TEST_NUMPY,
+        TEST_DATAARRAY,
+        TEST_DATASET,
+    ],
+)
+@pytest.mark.parametrize(
+    "param",
+    [
+        1,
+        "2",
+        # 3.  # TODO: Not yet implemented
+    ],
+)
 def test_format_handler_multiple_args_kwargs(in_data, param):
     """All in_data types are accepted, therefore no tranformation should be attempted."""
+
     @format_handler()
     def _xarray_handler(
         data1: xr.DataArray,
@@ -76,25 +97,31 @@ def test_format_handler_multiple_args_kwargs(in_data, param):
         param2: str,
     ):
         return data1, data2, param1, param2
-    out_data1, out_data2, out_param1, out_param2 = _xarray_handler(in_data, in_data, param, param)
+
+    out_data1, out_data2, _out_param1, _out_param2 = _xarray_handler(in_data, in_data, param, param)
     assert isinstance(out_data1, xr.DataArray)
     assert isinstance(out_data2, np.ndarray)
-    
+
     # TODO: The following are not yet implemented.
     # assert isinstance(out_param1, int)
     # assert isinstance(out_param2, str)
 
 
-@pytest.mark.parametrize("in_data", [
-    TEST_NUMPY,
-    TEST_DATAARRAY,
-    TEST_DATASET,
-])
+@pytest.mark.parametrize(
+    "in_data",
+    [
+        TEST_NUMPY,
+        TEST_DATAARRAY,
+        TEST_DATASET,
+    ],
+)
 def test_format_handler_with_kwarg_types(in_data):
     """All in_data types are accepted, therefore no tranformation should be attempted."""
+
     @format_handler(kwarg_types={"data": xr.DataArray})
     def _xarray_handler(data):
         return data
+
     assert isinstance(_xarray_handler(in_data), xr.DataArray)
 
 
@@ -115,6 +142,7 @@ def test_format_handler_no_earthkit_data(caplog, monkeypatch):
     @format_handler()
     def _handler(data: xr.DataArray):
         return data
+
     _handler(TEST_NUMPY)
     # Check that a warning/error was logged when incompatible type passed
     assert any(
